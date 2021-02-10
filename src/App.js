c09ae956f587ce8bb88dbfa59aaafef2dd3986af
Christie Jensen <christiejensen@Christies-MacBook-Air.local>
2021-02-10 12:19:27 -0700

    complete with single reducer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
+import { connect } from 'react-redux'
+import { loginUser } from './redux/reducer'
 
 class App extends Component{
   constructor(){
@@ -16,14 +18,30 @@ class App extends Component{
     })
   }  
 
+  handleClick = () => {
+    const { username, email } = this.state
+    let user = { username, email }
+    //below is my action creator from reducer.js Redux
+    this.props.loginUser(user)
+    this.setState({
+      username: '',
+      email: ''
+    })
+  }
+
   render(){
     return (
       <div className="App">
         <input onChange={ e => this.changeHandler(e)} name="username" type="text" value={this.state.username} placeholder="Username"/>
         <input onChange={ e => this.changeHandler(e)} name="email" type="text" value={this.state.email} placeholder="Email"/>
+        <button onClick={ this.handleClick }>Add User</button>
+        <h3>The username is: { this.props.user.username }</h3>
       </div>
     );
   }
 }
 
-export default App;
+//takes in Redux state, adds it to props so it can be used in this component (ex. line 38)
+const mapStateToProps = state => state
+
+export default connect(mapStateToProps, { loginUser })(App);
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -13,6 +13,8 @@ export function loginUser(user){
 
 export default function reducer(state = initialState, action){
     switch(action.type){
+        case LOGIN_USER:
+            return {...state, user: action.payload} 
         default: 
             return state
         }
